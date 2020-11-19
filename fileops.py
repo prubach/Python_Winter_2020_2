188c2a90553329e238eaba8db99b37e8852f421f
Pawel Rubach <pawel.rubach@sgh.waw.pl>
2020-11-19 12:58:10 +0100

    Dump a dictionary

--- fileops.py
+++ fileops.py
@@ -1,17 +1,20 @@
-arr = [456, 361, 424, 67, 661]
+arr_a = [456, 361, 424, 67, 661]
+
+
+arr = { 'one': 'Hello', 'two': 'Hi', 'three': arr_a }
 
 import _pickle
 
 import json
 
 fw = 't.dat'
-#with open(fw, 'wb') as outfile:
-#    _pickle.dump(arr, outfile)
+with open(fw, 'wb') as outfile:
+    _pickle.dump(arr, outfile)
 
 
-#with open(fw, 'rb') as infile:
-#    out_arr = _pickle.load(infile)
-#    print('Got arr from file: ' + str(out_arr))
+with open(fw, 'rb') as infile:
+    out_arr = _pickle.load(infile)
+    print('Got arr from file: ' + str(out_arr))
 
 
 fw = 't.json'
