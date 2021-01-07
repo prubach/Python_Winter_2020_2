09ca7aa707525368c08ee2d1274295a93c8d7c3e
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-01-07 12:30:14 +0100

    Threads - run in parallel and wait for all threads to finish

--- threads.py
+++ threads.py
@@ -18,4 +18,27 @@ class MyThread(threading.Thread):
 #print(time.ctime(time.time()))
 
 thread1 = MyThread('Thread 1', 1, 5)
-thread1.run()
+thread2 = MyThread('Thread 2', 1, 5)
+thread3 = MyThread('Thread 3', 1, 5)
+# Sequentially
+#thread1.run()
+#thread2.run()
+
+# in parallel
+thread1.start()
+thread2.start()
+thread3.start()
+
+#thread1.join()
+
+
+all_threads = []
+all_threads.append(thread1)
+all_threads.append(thread2)
+all_threads.append(thread3)
+
+for thr in all_threads:
+    thr.join()
+
+
+print('Exiting main thread')
