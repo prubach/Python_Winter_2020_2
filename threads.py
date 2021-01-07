0a2d61021b64abbac6f098a9d44fc768f4705e8d
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-01-07 12:37:07 +0100

    Threads - shared list

--- threads.py
+++ threads.py
@@ -1,25 +1,32 @@
 import threading
 import time
+import multiprocessing
 
 class MyThread(threading.Thread):
-    def __init__(self, thread_name, delay, counter):
+    def __init__(self, thread_name, delay, counter, shared_list):
         threading.Thread.__init__(self)
         self.thread_name = thread_name
         self.delay = delay
         self.counter = counter
+        self.shared_list = shared_list
 
     def run(self):
         print('Starting: ' + self.thread_name)
         while self.counter > 0:    #while self.counter:
             time.sleep(self.delay)
             print('%s: %s' % (self.thread_name, time.ctime(time.time())))
+            self.shared_list.append(self.thread_name + ': ' + str(self.counter))
             self.counter -= 1
+
+print('CPUs: %s' % (multiprocessing.cpu_count()))
 
 #print(time.ctime(time.time()))
 
-thread1 = MyThread('Thread 1', 1, 5)
-thread2 = MyThread('Thread 2', 1, 5)
-thread3 = MyThread('Thread 3', 1, 5)
+shared_list = []
+
+thread1 = MyThread('Thread 1', 1, 5, shared_list)
+thread2 = MyThread('Thread 2', 1, 5, shared_list)
+thread3 = MyThread('Thread 3', 1, 5, shared_list)
 # Sequentially
 #thread1.run()
 #thread2.run()
@@ -39,6 +46,7 @@ all_threads.append(thread3)
 
 for thr in all_threads:
     thr.join()
-
+# Thread safety !!!
+print('Shared list: ' + str(shared_list))
 
 print('Exiting main thread')
